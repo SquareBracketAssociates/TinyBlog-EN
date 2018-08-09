0cd55b222d4200d53598e87c4cb19efa88bf2995
StéphaneDucasse <stephane.ducasse@inria.fr>
2018-08-09 15:22:53 +0200

    first chapter in english

--- download.sh
+++ download.sh
@@ -3,6 +3,7 @@ PHARO_VM=${PHARO_VM:-./pharo}
 
 pillar_version="stable"
 pharo_version="50"
+#Pay attention below we have v5.0 to be changed too
 vm_enable=0
 img_enable=0
 
@@ -90,7 +91,10 @@ if [[ $vm_enable == 0 && $img_enable == 0 ]]; then
     img_enable=1
 fi
 
-IMAGE_URL="https://github.com/pillar-markup/pillar/releases/download/$pharo_version/Pillar-deployment$pharo_version.zip";
+#IMAGE_URL="https://github.com/pillar-markup/pillar/releases/download/$pharo_version/Pillar-deployment$pharo_version.zip";
+IMAGE_URL="https://github.com/pillar-markup/pillar/releases/download/v5.0/Pillar-deployment$pharo_version.zip"; 
+#https://github.com/pillar-markup/pillar/releases/download/v5.0/Pillar-deployment50.zip
+
 VM_INSTALL_URL="http://get.pharo.org/vm$pharo_version"
 
 if [[ $vm_enable == 1 || $img_enable == 0 ]]; then
